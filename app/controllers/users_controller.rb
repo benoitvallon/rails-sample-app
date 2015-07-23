107e61708204f80a6a0281c0344329b983447df0
Benoit VALLON <benoitvallon@gmail.com>
2015-07-23 17:13:31 -0400

    Adds welcome message for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
