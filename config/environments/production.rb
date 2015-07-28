b2736397b8f56483bab99a0702b91a4a7410bd27
Benoit VALLON <benoitvallon@gmail.com>
2015-07-28 17:17:51 -0400

    Fixes email production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,9 +62,9 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'powerful-ocean-1964.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
