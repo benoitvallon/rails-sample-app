fa423e76f40ba5cc178a9f414c5c4906f6216012
Benoit VALLON <benoitvallon@gmail.com>
2015-07-28 15:26:54 -0400

    Adds the micropost resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  has_many :microposts, dependent: :destroy
 
   # Returns the hash digest of the given string.
   def User.digest(string)
